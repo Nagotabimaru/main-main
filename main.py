0d3c948ea825051e8ec35d1eca8d320811779a61
Nagotabimaru <kruz.r@list.ru>
2023-01-31 09:21:20 +0400

    adding division

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 a = int(input("Введите Число: "))
 b = int(input("Введите Число: "))
 simvol = input("Введите символ: ")
-resault = 0
+result = 0
 
 if simvol == "+":
-    resault = a+b
-    print(resault)
+    result = a+b
+    print(result)
 elif simvol == "-":
-    resault = a-b
-    print(resault)
+    result = a-b
+    print(result)
 elif simvol == "*":
-    resault = a*b
-    print(resault)
+    result = a*b
+    print(result)
+elif simvol == "/":
+    result = a/b
+    print(result)
